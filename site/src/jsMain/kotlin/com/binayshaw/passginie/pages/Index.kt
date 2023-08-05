c473411bb7c14fc4d79c96e665854478c9383003
Binay Shaw <binayshaw7777@gmail.com>
2023-08-05 18:58:54 +0530

    Password customization clickables

--- site/src/jsMain/kotlin/com/binayshaw/passginie/pages/Index.kt
+++ site/src/jsMain/kotlin/com/binayshaw/passginie/pages/Index.kt
@@ -3,9 +3,10 @@ package com.binayshaw.passginie.pages
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
 import com.binayshaw.passginie.components.layouts.PageLayout
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.silk.components.forms.Button
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.CheckboxInput
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 import passGenerator
@@ -17,15 +18,99 @@ fun HomePage() {
         val generatedPassword = remember {
             mutableStateOf("Fd9)4y3g")
         }
+        val passwordLength = remember {
+            mutableStateOf(8)
+        }
+        val shouldIncludeUppercase = remember {
+            mutableStateOf(false)
+        }
+        val shouldIncludeLowercase = remember {
+            mutableStateOf(true)
+        }
+        val shouldIncludeNumbers = remember {
+            mutableStateOf(false)
+        }
+        val shouldIncludeSymbols = remember {
+            mutableStateOf(false)
+        }
+        P()
+        Text("Password length: ${passwordLength.value}")
+        P()
+        Row {
+            Button(onClick = {
+                if (passwordLength.value > 4) {
+                    passwordLength.value -= 1
+                }
+            }) {
+                Text("-")
+            }
+
+            Button(onClick = {
+                if (passwordLength.value < 15) {
+                    passwordLength.value += 1
+                }
+            }) {
+                Text("+")
+            }
+        }
+        P()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            CheckboxInput(
+                checked = shouldIncludeUppercase.value,
+                attrs = {
+                    onClick {
+                        shouldIncludeUppercase.value = shouldIncludeUppercase.value.not()
+                    }
+                }
+            )
+            Text("Include Uppercase")
+        }
+        P()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            CheckboxInput(
+                checked = shouldIncludeLowercase.value,
+                attrs = {
+                    onClick {
+                        shouldIncludeLowercase.value = shouldIncludeLowercase.value.not()
+                    }
+                }
+            )
+            Text("Include Lowercase")
+        }
+        P()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            CheckboxInput(
+                checked = shouldIncludeNumbers.value,
+                attrs = {
+                    onClick {
+                        shouldIncludeNumbers.value = shouldIncludeNumbers.value.not()
+                    }
+                }
+            )
+            Text("Include Numbers")
+        }
+        P()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            CheckboxInput(
+                checked = shouldIncludeSymbols.value,
+                attrs = {
+                    onClick {
+                        shouldIncludeSymbols.value = shouldIncludeSymbols.value.not()
+                    }
+                }
+            )
+            Text("Include Symbols")
+        }
+
         P()
         Button(
             onClick = {
                 generatedPassword.value = passGenerator(
-                    8,
-                    false,
-                    false,
-                    false,
-                    false
+                    passwordLength.value,
+                    shouldIncludeUppercase.value,
+                    shouldIncludeLowercase.value,
+                    shouldIncludeNumbers.value,
+                    shouldIncludeSymbols.value
                 )
             }
         ) {
